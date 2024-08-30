681da263ebf2afbb0b4096422fd6d99d21d6dfa5
Aadya-programmer <aadyagoel2003@gmail.com>
2024-08-30 15:42:07 +0530

    changes made

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,21 +2,55 @@ import { Header } from "../Reusable/Header"
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { Container, Row, Col } from "react-bootstrap";
+// import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import bg from './bg.png';
 export function Homepage(){
   return (
-  <div>
-    <div className="reader-den">
-    <Header/>
-    <div className="btn">
-    <Stack spacing={2} direction="row">
-      <Button variant="contained" style={{backgroundColor:"grey"}}>Read Now</Button>
-    </Stack>
-    </div>
-  </div>
-  <div className="total-readers">
-    
-  </div>
-  </div>
-  
+    <>
+    <Container fluid >
+      <Row>
+      <Col style={{
+        paddingLeft:'0px',
+        paddingRight:'0px'
+      }}>
+      <Header />
+      </Col>
+      </Row>
+      <Row>
+        <Col style={{paddingLeft:'0px', paddingRight:'0px'}}>
+          <div
+          style={{
+            height: '600px',
+            backgroundImage: `url(${bg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            display: 'flex',
+            marginTop:'60px'
+            // position: 'absolute',
+            // width: '100%',
+            // height: '100%',
+            // backgroundImage: `url(${bg})`,
+            // backgroundSize: 'cover',
+            // backgroundPosition: 'center'
+          }}>
+            <Stack>
+              <Button variant="contained" style={{marginLeft:'92px', marginTop:'170%', backgroundColor:'grey'}}>
+                Explore Now
+              </Button>
+            </Stack>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+    <Container fluid>
+      <Row className="total-readers">
+        <Col>
+          <img src="ppl.png" alt='pic' height={100} width={200}></img>
+        </Col>
+      </Row>
+
+    </Container>
+    </>
   );
 }
--- src/Reusable/Header.js
+++ src/Reusable/Header.js
@@ -5,11 +5,12 @@ export function Header(){
     <div className="top">
       <Navbar data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home" style={{color:"black", fontSize:"20px"}}><HomeRoundedIcon/></Navbar.Brand>
+          <Navbar.Brand href="#home" style={{color:"white", fontSize:"20px"}}><HomeRoundedIcon/></Navbar.Brand>
           <Nav className="me-auto"> &emsp;
-            <Nav.Link href="#home" style={{color:"black", fontSize:"20px"}}>Explore</Nav.Link> &emsp;&emsp;
-            <Nav.Link href="#recommendations" style={{color:"black", fontSize:"20px"}}>Recommendations</Nav.Link>&emsp;&emsp;
-            <Nav.Link href="#about" style={{color:"black", fontSize:"20px"}}>About Us</Nav.Link>
+            <Nav.Link href="#home" style={{color:"white", fontSize:"20px"}}>Explore</Nav.Link> &emsp;&emsp;
+            <Nav.Link href="#recommendations" style={{color:"white", fontSize:"20px"}}>Recommendations</Nav.Link>&emsp;&emsp;
+            <Nav.Link  href="#about" style={{color:"white", fontSize:"20px"}}>About Us</Nav.Link>&emsp;&emsp;
+            <Nav.Link  href="#about" style={{color:"white", fontSize:"20px"}}>Contact Us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
